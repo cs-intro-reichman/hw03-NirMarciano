ea6a9d19a405afdf721d383b372572cb3810b93d
NirMarciano <nirdavid.marciano@post.runi.ac.il>
2024-01-06 01:09:38 +0200

    Upload 3.0

--- Calendar1.java
+++ Calendar1.java
@@ -1,29 +1,43 @@
-public class Calendar1 {
-
-    static int dayOfMonth = 1;   
+/** 
+ * Prints the calendars of all the years in the 20th century.
+ */
+public class Calendar1 {	
+    // Starting the calendar on 1/1/1900
+	static int dayOfMonth = 1;   
 	static int month = 1;
 	static int year = 1900;
-    static int dayOfWeek = 2; 
-	static int nDaysInMonth = 31;
+	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
+	static int nDaysInMonth = 31; // Number of days in January
+	
+	/** 
+	 * Prints the calendars of all the years in the 20th century. Also prints the  
+	 * number of Sundays that occured on the first day of the month during this period.
+	 */
+	public static void main(String args[]) {
+		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
+	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
+	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
 
-    public static void main(String[] args) {
-        
-        int inYear = Integer.parseInt(args[0]); 
-
-	 	while (year != inYear) {
-	 		advance();
-        }
-
-        while(year != (inYear +1)) {
-        System.out.print(dayOfMonth + "/" + month + "/" + year);
-        if(dayOfWeek == 1) System.out.println(" Sunday");
+	    int SundayFirst = 0;
+	 	while (year != 2000) {
+	 		System.out.print(dayOfMonth + "/" + month + "/" + year);
+			if(dayOfWeek == 1) System.out.println(" Sunday");
 			else System.out.println();
-        advance();
+	 		advance();
+			if(dayOfMonth == 1 && dayOfWeek == 1) SundayFirst++;
+	 		//// If you want to stop the loop after n days, replace the condition of the
+	 		//// if statement with the condition (debugDaysCounter == n)
+	 		//if (debugDaysCounter == 36500) { 
+	 		//	break;
+	 		//}
         }
-    
-    }
-
-    private static void advance() {
+		System.out.println("During the 20th century, " + SundayFirst +  " Sundays fell on the first day of the month");
+	 }
+	
+	 // Advances the date (day, month, year) and the day-of-the-week.
+	 // If the month changes, sets the number of days in this month.
+	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
+	 private static void advance() {
 
 		if(dayOfMonth == nDaysInMonth(month, year))	{
 		dayOfMonth = 1;
@@ -36,11 +50,12 @@ public class Calendar1 {
 		else {
 			dayOfMonth++;
 		}
-        if(dayOfWeek == 7) dayOfWeek =1;
+		if(dayOfWeek == 7) dayOfWeek =1;
 		else dayOfWeek++;
 	 }
-
-     private static boolean isLeapYear(int year) {
+		 
+    // Returns true if the given year is a leap year, false otherwise.
+	private static boolean isLeapYear(int year) {
 		if(year % 400 == 0)
 			return true;
 		else if(year % 4 == 0 && year % 100 == 0)
@@ -50,8 +65,12 @@ public class Calendar1 {
 
 	return false;
 	}
-
-    private static int nDaysInMonth(int month, int year) {
+	 
+	// Returns the number of days in the given month and year.
+	// April, June, September, and November have 30 days each.
+	// February has 28 days in a common year, and 29 days in a leap year.
+	// All the other months have 31 days.
+	private static int nDaysInMonth(int month, int year) {
 		switch(month) {
 			case 1: return nDaysInMonth;
 			case 3: return nDaysInMonth;
@@ -67,8 +86,9 @@ public class Calendar1 {
 			case 2: { 
 				if(isLeapYear(year)) return nDaysInMonth - 2;
 					else return nDaysInMonth - 3;
-		        }
-            default: return 0;
-	    }
-    }
+			}
+			default: return 0;
+		}
+
+	}
 }
